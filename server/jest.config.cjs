bcb55d80ca7d465e5502de6f74144d701ff455f6
Denys  Morys <denys.morys.23@pnu.edu.ua>
2026-02-07 00:58:01 +0200

    Added jest test for request service

---
+++ server/jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
---
+++ server/src/services/__tests__/request.service.test.ts
@@ -0,0 +1,52 @@
+// Jest unit test for request.service - mocks Request model methods
+import { requestService } from "../request.service";
+
+jest.mock("../../models/Request", () => ({
+  __esModule: true,
+  default: {
+    find: jest.fn(),
+    countDocuments: jest.fn(),
+  },
+}));
+
+const RequestModel = require("../../models/Request").default;
+
+describe("requestService.getList (unit)", () => {
+  beforeEach(() => {
+    // mock chainable find().populate().sort().skip().limit().lean()
+    (RequestModel.find as jest.Mock).mockImplementation(() => ({
+      populate: () => ({
+        sort: () => ({
+          skip: () => ({
+            limit: () => ({
+              lean: async () => [
+                {
+                  _id: "1",
+                  title: "Test",
+                  description: "desc",
+                  subject: "math",
+                  author: { name: "User" },
+                },
+              ],
+            }),
+          }),
+        }),
+      }),
+    }));
+
+    (RequestModel.countDocuments as jest.Mock).mockResolvedValue(1);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("returns items and pagination", async () => {
+    const res = await requestService.getList({ page: 1, limit: 10 });
+    expect(res).toHaveProperty("items");
+    expect(res).toHaveProperty("pagination");
+    expect(res.pagination.total).toBe(1);
+    expect(res.items.length).toBe(1);
+    expect(res.items[0].title).toBe("Test");
+  });
+});
